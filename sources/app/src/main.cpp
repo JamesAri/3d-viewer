bed7211878e2fb4d0424ebb2c7aa2b785b50f592
Jakub Šlechta <jakub.slechta@emplifi.io>
2026-09-07 15:58:48 +0200

    [build] Added basic cmake setup

---
+++ sources/app/src/main.cpp
@@ -0,0 +1,8 @@
+#include <print>
+#include "window.h"
+
+int main() {
+    std::println("[main] C++23");
+	window();
+	return 0;
+}
---
+++ sources/window/src/window.cpp
@@ -0,0 +1,6 @@
+#include <print>
+#include "window.h"
+
+void window() {
+    std::println("[window] C++23");
+}
