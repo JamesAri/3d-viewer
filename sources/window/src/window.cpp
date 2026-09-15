f257e91c2055435916983b577c0b1c44b7ed6647
Jakub Šlechta <jakub.slechta@emplifi.io>
2026-09-15 16:05:06 +0200

    [Window] Added basic SFML example - render window

--- sources/window/src/window.cpp
+++ sources/window/src/window.cpp
@@ -1,6 +1,22 @@
 #include <print>
 #include "window.h"
+#include <SFML/Graphics.hpp>
 
 void window() {
-    std::println("[window] C++23");
+    sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
+	sf::CircleShape shape( 100.f );
+	shape.setFillColor( sf::Color::Green );
+
+	while ( window.isOpen() )
+	{
+		while ( const std::optional event = window.pollEvent() )
+		{
+			if ( event->is<sf::Event::Closed>() )
+				window.close();
+		}
+
+		window.clear();
+		window.draw( shape );
+		window.display();
+	}
 }
